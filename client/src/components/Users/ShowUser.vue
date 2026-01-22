2b87aa4e1cce2c0672c2d3372339d81a0462f700
- <->
2026-01-22 16:56:21 +0700

    Lab8

--- client/src/components/Users/ShowUser.vue
+++ client/src/components/Users/ShowUser.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
     <h1>Show User</h1>
-    <p>User ID: {{ userId }}</p>
+    <div v-if="user">
+      <p>id: {{ user.id }}</p>
+      <p>ชื่อ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+      <p>email: {{ user.email }}</p>
+      <p>password: {{ user.password }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import UsersService from '../../services/UsersService'
+
 export default {
-  // Logic จะเขียนตรงนี้
-  data () {
+  data() {
     return {
-      userId: null
+      user: null
     }
   },
-  created () {
-    // โค้ดนี้ถูกรันทันทีที่ Component ถูกสร้างขึ้น (ก่อนเมาท์)
-    this.userId = this.$route.params.userId 
-    // ณ จุดนี้ this.userId ได้รับค่าแล้ว และจะถูกใช้ในการ Render UI
-
+  async created() {
+    try {
+      let userId = this.$route.params.userId
+      this.user = (await UsersService.show(userId)).data
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 </script>
